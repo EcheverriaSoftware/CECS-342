14cd777a9f06ebbbe78eccad4b8118b8c6575621
Miguel <EcheverriaSoftware@gmail.com>
2022-02-13 14:30:15 -0800

    Quicksort now only accepts 2 arguements

--- Quicksort/Quicksort.c
+++ Quicksort/Quicksort.c
@@ -1,32 +1,40 @@
 #include <unistd.h>
 #include <stdio.h>
 
-void quicksort(int *arr, int low, int high);
+void quicksort(int *arr, int n);
+void quicksortHelper(int *arr, int low, int high);
 int partition(int *arr, int low, int high);
-void swap(int * arr, int i, int j);
+void swap(int *arr, int i, int j);
 
 int main(int argc, char *argv[])
 {
-    int arr [8] = {34, 23, 12, 3, 4, 5, 1, 0};
+    int arr[8] = {34, 23, 12, 3, 24, 5, 1, -1};
     int length = (sizeof(arr) / sizeof(arr[0])) - 1;
-    quicksort(arr, 0, length);
-    for(int i = 0; i < length + 1; i++)
+    quicksort(arr, length);
+    for (int i = 0; i < length + 1; i++)
     {
         printf("%d", arr[i]), "\n";
         printf("\n");
     }
     return 0;
 }
-void quicksort(int *arr, int low, int high)
+void quicksort(int *arr, int n)
 {
-    if(high == 0 || high == -1){
+    int low = 0;
+    int high = n;
+    quicksortHelper(arr, low, high);
+}
+void quicksortHelper(int *arr, int low, int high)
+{
+    if (high == 0 || high == -1)
+    {
         return;
     }
     if (low < high)
     {
         int pivot = partition(arr, low, high);
-        quicksort(arr, low, pivot - 1);
-        quicksort(arr, pivot + 1, high);
+        quicksortHelper(arr, low, pivot - 1);
+        quicksortHelper(arr, pivot + 1, high);
     }
 }
 int partition(int *arr, int low, int high)
@@ -44,7 +52,8 @@ int partition(int *arr, int low, int high)
     swap(arr, i - 1, low);
     return i - 1;
 }
-void swap(int *arr, int i, int j){
+void swap(int *arr, int i, int j)
+{
     int temp = arr[i];
     arr[i] = arr[j];
     arr[j] = temp;
